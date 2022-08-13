fbb1ebd1badb555709239638f7c2e3309b93be8f
LMNatale1967 <LuisMiguelNatale@gmail.comm>
2022-08-13 11:40:52 +0200

    New Comma After Save Changes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
 
   constructor() {}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
 export class Tab3Page {
 
   constructor() {}
